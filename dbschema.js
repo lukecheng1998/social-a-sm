1871bf947cff188f1251ef0c84d56dd0ad570d4f
lukecheng1998 <lukecheng1998@gmail.com>
2019-12-21 23:42:27 -0800

    sign up working

---
+++ dbschema.js
@@ -0,0 +1,12 @@
+//Reference only
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'scream body',
+            createdAt: '2019-03-15T23:53:0224',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
--- functions/index.js
+++ functions/index.js
@@ -1,36 +1,54 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-
+const app = require('express')();
 admin.initializeApp();
 
-const express = require('express');
-const app = express();
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDfmZZ_67XHjyg0weyHVLBsm-MuhTOpcz4",
+    authDomain: "social-a-f3a70.firebaseapp.com",
+    databaseURL: "https://social-a-f3a70.firebaseio.com",
+    projectId: "social-a-f3a70",
+    storageBucket: "social-a-f3a70.appspot.com",
+    messagingSenderId: "980869541447",
+    appId: "1:980869541447:web:f9809297bd1c4b6e5e4b62",
+    measurementId: "G-C7QJ4SXFR0"
+  };
+
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+
+const db = admin.firestore();
 //Gets the posts
-exports.getScreams = functions.https.onRequest((req, res) => {
-    admin
-    .firestore()
+app.get('/screams', (req, res) => {
+    db
     .collection('screams')
+    .orderBy('createdAt', 'desc')
     .get()
     .then((data) => {
         let screams = [];
         data.forEach((doc) => {
-            screams.push(doc.data());
+            screams.push({
+                screamId: doc.id,
+                body: doc.data().body,
+                userHandle: doc.data().userHandle,
+                createdAt: doc.data().createdAt
+            });
         });
         return res.json(screams);
     })
     .catch((err) => console.error(err));
 });
 //writes to posts
-exports.createScream =  functions.https.onRequest((req, res) => {
-    if(req.method !== 'POST'){
-        return res.status(400).json({rror: 'Method not allowed'});
-    }
+app.post('/scream', (req, res) => {
     const newScream = {
         body: req.body.body,
         userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     };
-    admin.firestore()
+    db
     .collection('screams')
     .add(newScream)
     .then(doc => {
@@ -41,3 +59,57 @@ exports.createScream =  functions.https.onRequest((req, res) => {
         console.log(err);
     })
 });
+
+//Signup route
+let token, userId;
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle
+    };
+    //TODO validate data
+
+    db.doc(`/users/${newUser.handle}`).get()
+    .then(doc => {
+        if(doc.exists){
+            //hopefully reroute this
+            return res.status(400).json({handle: 'user already exists'});
+        }else{
+            return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+        }
+    })
+    .then(data => {
+        //How to return access token
+        userId = data.user.uid;
+        return data.user.getIdToken();
+    })
+    .then(idToken => {
+        token = idToken;
+        const userCredentials = {
+            handle: newUser.handle,
+            email: newUser.email,
+            createdAt: new Date().toISOString(),
+            userId
+        };
+        return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
+        return res.status(201).json({token});
+    })
+    .catch(err => {
+        console.error(err);
+        if(err.code === 'auth/email-already-in-use'){
+            return res.status(400).json({email: 'Email is already in use'})
+        }else{
+            return res.status(500).json({error: err.code});
+        }
+        
+    })
+})
+
+
+//Have to export our changes for express
+//i.e. https://baseurl.com/url/...
+exports.api = functions.https.onRequest(app);
